c54ff3b02a3eaacd22f86898c994531e0fdd328a
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-06-30 01:35:48 +0530

    commit

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-white'>
+        <h1>SafeNest</h1>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Github</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
