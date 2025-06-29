3242309f125a79d3cdaad4c15d5e3951460e16db
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-06-30 01:49:45 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import {Navbar} from './Component/Navbar'
+import Navbar from './Component/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='text-white'>
-        <h1>SafeNest</h1>
+    <div className='text-white flex justify-between py-2 px-4  items-center'>
+        <h1 className='text-3xl'>Safe<span className='text-violet-600'>Nest</span></h1>
         <div>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Github</li>
+            <ul className='flex gap-4 font-semibold cursor-pointer '>
+                <li className='hover:text-violet-300 hover:scale-105 transition-all ease-in'>Home</li>
+                <li className='hover:text-violet-300 hover:scale-105 transition-all ease-in'>About</li>
+                <li className='hover:text-violet-300 hover:scale-105 transition-all ease-in'>Github</li>
             </ul>
         </div>
     </div>
