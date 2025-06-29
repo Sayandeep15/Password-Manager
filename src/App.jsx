9a8ef5c8be8ac2752fe36310ff61863ef1cc2d5a
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-06-30 04:13:03 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './Component/Navbar'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Component/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <Navbar/> 
+      <div className="w-full h-screen">
+        <Navbar />
+        <div class="relative w-fit">
+          <div class="absolute inset-0 rounded-full bg-blue-700 opacity-50 blur-3xl w-48 h-48"></div>
+
+          {/* <div class="w-40 h-40 bg-blue-500 rounded-full shadow-2xl"></div> */}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
