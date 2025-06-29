e8c980d1abfb8705517a6f8af1a5f0396c3db698
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-06-30 04:01:42 +0530

    commit

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { TbPasswordFingerprint } from "react-icons/tb";
+import { HiOutlineExternalLink } from "react-icons/hi";
 
 const Navbar = () => {
   return (
     <div className='text-white flex justify-between py-2 px-4  items-center'>
-        <h1 className='text-3xl'>Safe<span className='text-violet-600'>Nest</span></h1>
+        <h1 className='text-3xl flex'>Safe<span className='text-violet-600 flex flex-row gap-2 items-center'>Nest <TbPasswordFingerprint/></span></h1>
         <div>
             <ul className='flex gap-4 font-semibold cursor-pointer '>
                 <li className='hover:text-violet-300 hover:scale-105 transition-all ease-in'>Home</li>
                 <li className='hover:text-violet-300 hover:scale-105 transition-all ease-in'>About</li>
-                <li className='hover:text-violet-300 hover:scale-105 transition-all ease-in'>Github</li>
+                <li className='hover:text-violet-300 hover:scale-105 transition-all ease-in flex items-center gap-0.5'>Github<HiOutlineExternalLink /></li>
             </ul>
         </div>
     </div>
