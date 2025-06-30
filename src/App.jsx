646a0c26c91c7caf4ace249aed8c9aeaf2572b9b
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-06-30 21:52:16 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,36 @@ function App() {
         <div class="relative w-full">
           {/* BLUR CIRCLE */}
           <div class="absolute inset-0 rounded-full bg-violet-600 opacity-50 blur-3xl w-64 h-64 z-50 top-0 left-[90%]"></div>
-          <div class="absolute inset-0 rounded-full bg-violet-600 opacity-50 blur-3xl w-64 h-64 z-50 top-0 left-[90%]"></div>
         </div>
         <Navbar />
         {/* INPUT AREA */}
         <div className="w-[90%]  mx-auto items-center flex flex-col justify-center my-8 gap-4">
-          <h1 className="text-3xl text-white ">Manage Your<span className="text-violet-600  items-center">{" "}PASSWORD{" "}</span>{" "}At One Place</h1>
-          <input placeholder="Enter your url" type="text" className="w-[60%] border-2 border-violet-600 rounded-full p-1 bg-white" />
+          <h1 className="text-3xl text-white ">
+            Manage Your
+            <span className="text-violet-600  items-center"> PASSWORD </span> At
+            One Place
+          </h1>
+          <input
+            placeholder="Enter your url"
+            type="text"
+            className="w-[60%] border-2 border-violet-600 rounded-full p-1 bg-white"
+          />
 
           <div className="w-[60%] flex gap-2 justify-between">
-            <input placeholder="website" type="text" className="w-96 border-2 border-violet-600 rounded-full p-1 bg-white" />
-            <input placeholder="password" type="password" className=" w-96 border-2 border-violet-600 rounded-full p-1 bg-white" />
+            <input
+              placeholder="website"
+              type="text"
+              className="w-124 border-2 border-violet-600 rounded-full p-1 bg-white"
+            />
+            <input
+              placeholder="password"
+              type="password"
+              className=" w-124 border-2 border-violet-600 rounded-full p-1 bg-white"
+            />
           </div>
         </div>
-
-       </div> {/*MAIN DIV */}
+      </div>{" "}
+      {/*MAIN DIV */}
     </>
   );
 }
