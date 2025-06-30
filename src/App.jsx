9fa99933ce650313bf82fd9669a512cf34adbe43
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-06-30 21:45:39 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,24 @@ function App() {
   return (
     <>
       <div className="w-full h-screen">
-        <Navbar />
         <div class="relative w-full">
-          <div class="absolute inset-0 rounded-full bg-blue-700 opacity-50 blur-3xl w-48 h-48 z-50 top-0 left-[90%]"></div>
+          {/* BLUR CIRCLE */}
+          <div class="absolute inset-0 rounded-full bg-violet-600 opacity-50 blur-3xl w-64 h-64 z-50 top-0 left-[90%]"></div>
+          <div class="absolute inset-0 rounded-full bg-violet-600 opacity-50 blur-3xl w-64 h-64 z-50 top-0 left-[90%]"></div>
+        </div>
+        <Navbar />
+        {/* INPUT AREA */}
+        <div className="w-[90%]  mx-auto items-center flex flex-col justify-center my-8 gap-4">
+          <h1 className="text-3xl text-white ">Manage Your<span className="text-violet-600  items-center">{" "}PASSWORD{" "}</span>{" "}At One Place</h1>
+          <input placeholder="Enter your url" type="text" className="w-[60%] border-2 border-violet-600 rounded-full p-1 bg-white" />
 
+          <div className="w-[60%] flex gap-2 justify-between">
+            <input placeholder="website" type="text" className="w-96 border-2 border-violet-600 rounded-full p-1 bg-white" />
+            <input placeholder="password" type="password" className=" w-96 border-2 border-violet-600 rounded-full p-1 bg-white" />
+          </div>
         </div>
 
-      </div>
+       </div> {/*MAIN DIV */}
     </>
   );
 }
