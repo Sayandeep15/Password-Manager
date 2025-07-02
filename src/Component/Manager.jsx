e59b9212f3f84db9607211b89c5fc0199153c027
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-07-02 19:20:08 +0530

    commit

--- src/Component/Manager.jsx
+++ src/Component/Manager.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 import Button from "./Button";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import { MdContentCopy } from "react-icons/md";
+import { v4 as uuidv4 } from "uuid";
+import { ToastContainer, toast } from "react-toastify";
 
 const Manager = () => {
   const [show, setshow] = useState(false);
@@ -22,13 +25,16 @@ const Manager = () => {
     data.site.length < 5 ||
     data.username.length < 3 ||
     data.password.length < 8;
+
   //SAVE PASSWORD TO THE LOCAL STORAGE
   const handleSave = () => {
-    setpasswordArray([...passwordArray, data]);
-    localStorage.setItem("password", JSON.stringify([...passwordArray, data]));
+    setpasswordArray([...passwordArray, { ...data, id: uuidv4() }]);
+    localStorage.setItem(
+      "password",
+      JSON.stringify([...passwordArray, { ...data, id: uuidv4() }])
+    );
     console.log([...passwordArray, data]);
     setdata({ site: "", username: "", password: "" });
-    
   };
 
   const handleChange = (e) => {
@@ -39,8 +45,36 @@ const Manager = () => {
     setshow(!show);
   };
 
+  const copyText = (text) => {
+    toast("Coppied to clipboard!", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      
+    });
+    navigator.clipboard.writeText(text);
+  };
+
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        
+      />
       <div className="inputs w-[90%]  mx-auto items-center flex flex-col justify-center my-8 gap-4">
         <h1 className="text-3xl text-white ">
           Manage Your
@@ -86,11 +120,7 @@ const Manager = () => {
             </button>
           </div>
         </div>
-        <Button
-          value="SUBMIT"
-          onClick={handleSave}
-          disabled={isDisabled}
-        />
+        <Button value="SAVE" onClick={handleSave} disabled={isDisabled} />
         {/* <button className="text-white border-1 border-violet-600 rounded-full p-1 font-semibold cursor-pointer "> Add Password</button> */}
 
         {/* Input div ends--- */}
@@ -108,58 +138,76 @@ const Manager = () => {
             </div>
           )}
           {/* TABLE */}
-          {passwordArray.length>=1 &&(
-          <div className="max-h-96 overflow-y-auto rounded-md ">
-            <table className="table-auto w-full text-center  ">
-              <thead className="bg-violet-700 sticky top-0 z-10">
-                <tr>
-                  <th className="w-36 py-1">Website</th>
-                  <th className="w-36 py-1">Username</th>
-                  <th className="w-36 py-1">Password</th>
-                </tr>
-              </thead>
-              <tbody>
-                {passwordArray.map((items, index) => (
-                  <tr key={index} className="bg-[#0a0a14] ">
-                    <td
-                      className=" "
-                      style={{
-                        boxShadow:
-                          "inset 0 1px 3px rgba(124, 58, 237, 0.08), inset 0 0 2px rgba(255, 255, 255, 0.03)",
-                      }}
-                    >
-                      <a
-                        href={items.site}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {items.site}
-                      </a>
-                    </td>
-                    <td
-                      className="p-1.5"
-                      style={{
-                        boxShadow:
-                          "inset 0 1px 3px rgba(124, 58, 237, 0.08), inset 0 0 2px rgba(255, 255, 255, 0.03)",
-                      }}
-                    >
-                      {items.username}
-                    </td>
-                    <td
-                      className="p-1.5"
-                      style={{
-                        boxShadow:
-                          "inset 0 1px 3px rgba(124, 58, 237, 0.08), inset 0 0 2px rgba(255, 255, 255, 0.03)",
-                      }}
-                    >
-                      {items.password}
-                    </td>
+          {passwordArray.length >= 1 && (
+            <div className="max-h-96 overflow-y-auto rounded-md ">
+              <table className="table-auto w-full text-center  ">
+                <thead className="bg-violet-700 sticky top-0 z-10">
+                  <tr>
+                    <th className="w-36 py-1">Website</th>
+                    <th className="w-36 py-1">Username</th>
+                    <th className="w-36 py-1">Password</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>)}
-        </div> 
+                </thead>
+                <tbody>
+                  {passwordArray.map((items, index) => (
+                    <tr key={index} className="bg-[#0a0a14] text-sm">
+                      <td
+                        className=" "
+                        style={{
+                          boxShadow:
+                            "inset 0 1px 3px rgba(124, 58, 237, 0.08), inset 0 0 2px rgba(255, 255, 255, 0.03)",
+                        }}
+                      >
+                        <a
+                          href={items.site}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {items.site}
+                        </a>
+                      </td>
+                      <td
+                        className="p-1.5 "
+                        style={{
+                          boxShadow:
+                            "inset 0 1px 3px rgba(124, 58, 237, 0.08), inset 0 0 2px rgba(255, 255, 255, 0.03)",
+                        }}
+                      >
+                        <div className="flex items-center flex-row gap-1 justify-between">
+                          {items.username}
+                          <MdContentCopy
+                            onClick={() => {
+                              copyText(items.username);
+                            }}
+                            className="text-violet-500"
+                          />
+                        </div>
+                      </td>
+                      <td
+                        className="p-1.5 "
+                        style={{
+                          boxShadow:
+                            "inset 0 1px 3px rgba(124, 58, 237, 0.08), inset 0 0 2px rgba(255, 255, 255, 0.03)",
+                        }}
+                      >
+                        {" "}
+                        <div className="flex items-center flex-row gap-1 justify-between">
+                          {items.password}
+                          <MdContentCopy
+                            onClick={() => {
+                              copyText(items.password);
+                            }}
+                            className="text-violet-500"
+                          />
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
