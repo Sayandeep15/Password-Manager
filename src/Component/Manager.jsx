9203d52b26b1f869bc71a18a607410a74a294697
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-07-03 00:49:51 +0530

    commit

--- src/Component/Manager.jsx
+++ src/Component/Manager.jsx
@@ -55,7 +55,6 @@ const Manager = () => {
       draggable: true,
       progress: undefined,
       theme: "light",
-      
     });
     navigator.clipboard.writeText(text);
   };
@@ -73,7 +72,6 @@ const Manager = () => {
         draggable
         pauseOnHover
         theme="light"
-        
       />
       <div className="inputs w-[90%]  mx-auto items-center flex flex-col justify-center my-8 gap-4">
         <h1 className="text-3xl text-white ">
@@ -126,7 +124,7 @@ const Manager = () => {
         {/* Input div ends--- */}
 
         {/* PASSWORD TABLE */}
-        <div className="tableContainer w-[60%] mt-4 mx-auto items-center text-white ">
+        <div className="tableContainer w-[80%] mt-4 mx-auto items-center text-white ">
           <h1 className="text-xl mb-2">
             Your{" "}
             <span className="text-violet-600  items-center">Passwords</span>{" "}
@@ -140,19 +138,20 @@ const Manager = () => {
           {/* TABLE */}
           {passwordArray.length >= 1 && (
             <div className="max-h-96 overflow-y-auto rounded-md ">
-              <table className="table-auto w-full text-center  ">
-                <thead className="bg-violet-700 sticky top-0 z-10">
+              <table className="table-fixed w-full text-center  ">
+                <thead className="bg-violet-700 sticky top-0 z-10 ">
                   <tr>
-                    <th className="w-36 py-1">Website</th>
-                    <th className="w-36 py-1">Username</th>
-                    <th className="w-36 py-1">Password</th>
+                    <th className="w-[1/4] py-1">Website</th>
+                    <th className="w-[1/4] py-1">Username</th>
+                    <th className="w-[1/4] py-1">Password</th>
+                    <th className="w-[1/4] py-1">Action</th>
                   </tr>
                 </thead>
                 <tbody>
                   {passwordArray.map((items, index) => (
                     <tr key={index} className="bg-[#0a0a14] text-sm">
                       <td
-                        className=" "
+                        className="overflow-hidden "
                         style={{
                           boxShadow:
                             "inset 0 1px 3px rgba(124, 58, 237, 0.08), inset 0 0 2px rgba(255, 255, 255, 0.03)",
@@ -173,13 +172,13 @@ const Manager = () => {
                             "inset 0 1px 3px rgba(124, 58, 237, 0.08), inset 0 0 2px rgba(255, 255, 255, 0.03)",
                         }}
                       >
-                        <div className="flex items-center flex-row gap-1 justify-between">
+                        <div className="flex items-center flex-row gap-1 justify-between overflow-hidden relative">
                           {items.username}
                           <MdContentCopy
                             onClick={() => {
                               copyText(items.username);
                             }}
-                            className="text-violet-500"
+                            className="text-violet-500 absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
                           />
                         </div>
                       </td>
@@ -191,13 +190,13 @@ const Manager = () => {
                         }}
                       >
                         {" "}
-                        <div className="flex items-center flex-row gap-1 justify-between">
+                        <div className="flex items-center flex-row gap-1 justify-between overflow-hidden relative">
                           {items.password}
                           <MdContentCopy
                             onClick={() => {
                               copyText(items.password);
                             }}
-                            className="text-violet-500"
+                            className="text-violet-500 absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
                           />
                         </div>
                       </td>
