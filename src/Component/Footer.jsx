c3267e6f8db199d6c51fa0cb294f3fbdbd65b799
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-07-09 16:55:23 +0530

    commit

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -8,7 +8,7 @@ import { FaRegCopyright } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div className=' bg-[#0c011d8f] w-full text-white fixed mt-4 top-[91%] transform translate-y-1/2 flex justify-around py-2.5'>
-        <p className='text-xs flex items-center gap-1'><FaRegCopyright />2025 | Developed by Syanadeep Dutta  </p>
+        <p className='text-xs flex items-center gap-1'><FaRegCopyright />2025 | Developed by Sayandeep Dutta  </p>
         <ul className='flex items-center gap-4 font-semibold cursor-pointer '>
             <li className=' hover:scale-110 transition-all ease-in '><a className=' ' target='_blank' href="https://github.com/Sayandeep15/Password-Manager"><ImGithub /></a></li>
             <li className=' hover:scale-110 transition-all ease-in '><a className=' ' target='_blank' href="https://www.linkedin.com/in/sayandeep-dutta-profile/"><FaLinkedinIn /></a></li>
